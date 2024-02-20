b5a12c05c55fbac2b0078c48c747fc1c25af5022
Sameeurrehman <sameeurrehman@codistan.org>
2024-02-21 00:13:49 +0500

    created Login Page and SingUp Page

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
+import styles from "./Login.module.css";
+import TextInput from "../../components/TextInput/TextInput";
+
 
 const Login = () => {
   return (
-    <div>
-      Login123
+    <div className={styles.loginWrapper}>
+      <div className={styles.loginHeader}>Log in to your account</div>
+      <TextInput type="text" name="username" placeholder="username" />
+      <TextInput type="password" name="password" placeholder="password" />
+      <button className={styles.logInButton}>Log In</button>
+      <span>
+        Don't have an account?{" "}
+        <button className={styles.createAccount}>Register</button>
+      </span>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import styles from './Signup.module.css'
+import TextInput from "../../components/TextInput/TextInput";
 
 const Signup = () => {
   return (
-    <div>
-      Signup123
-    </div>
-  )
-}
+    <div className={styles.signupWrapper}>
+      <div className={styles.signupHeader}>Create an account</div>
+      <TextInput type="text" name="name" placeholder="name" />
 
-export default Signup
+      <TextInput type="text" name="username" placeholder="username" />
+
+      <TextInput type="text" name="email" placeholder="email" />
+
+      <TextInput type="password" name="password" placeholder="password" />
+
+      <TextInput
+        type="password"
+        name="confirmPassword"
+        placeholder="confirm password"
+      />
+
+      <button className={styles.signupButton}>Sign Up</button>
+
+      <span>
+        Already have an account?{" "}
+        <button className={styles.login}>Log In</button>
+      </span>
+
+      {/* {error != "" ? <p className={styles.errorMessage}>{error}</p> : ""} */}
+    </div>
+  );
+};
+
+export default Signup;
