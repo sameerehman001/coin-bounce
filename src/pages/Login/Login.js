c85984d2b786ce044a8ec696a176edd69a542805
Sameeurrehman <sameeurrehman@codistan.org>
2024-02-21 13:07:32 +0500

    logIn & signUp validation using formik, make Schema using yup

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,42 @@
 import React from "react";
 import styles from "./Login.module.css";
 import TextInput from "../../components/TextInput/TextInput";
-
+import loginSchema from "../../schemas/loginSchema";
+import { useFormik } from "formik";
 
 const Login = () => {
+  const { values, touched, handleBlur, handleChange, errors } = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+
+    validationSchema: loginSchema,
+  });
+
   return (
     <div className={styles.loginWrapper}>
       <div className={styles.loginHeader}>Log in to your account</div>
-      <TextInput type="text" name="username" placeholder="username" />
-      <TextInput type="password" name="password" placeholder="password" />
+      <TextInput
+        type="text"
+        name="username"
+        placeholder="username"
+        value={values.username}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        error={errors.username && touched.username ? 1 : undefined}
+        errormessage={errors.username}
+      />
+      <TextInput
+        type="password"
+        name="password"
+        placeholder="password"
+        value={values.password}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        error={errors.password && touched.password ? 1 : undefined}
+        errormessage={errors.password}
+      />
       <button className={styles.logInButton}>Log In</button>
       <span>
         Don't have an account?{" "}
--- src/pages/Signup/Signup.js
+++ src/pages/Signup/Signup.js
@@ -1,23 +1,76 @@
 import React from "react";
-import styles from './Signup.module.css'
+import styles from "./Signup.module.css";
+import signupSchema from "../../schemas/signupSchema";
+import { useFormik } from "formik";
 import TextInput from "../../components/TextInput/TextInput";
 
 const Signup = () => {
+  const { values, touched, handleBlur, handleChange, errors } = useFormik({
+    initialValues: {
+      name: "",
+      username: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+
+    validationSchema: signupSchema,
+  });
+
   return (
     <div className={styles.signupWrapper}>
       <div className={styles.signupHeader}>Create an account</div>
-      <TextInput type="text" name="name" placeholder="name" />
-
-      <TextInput type="text" name="username" placeholder="username" />
-
-      <TextInput type="text" name="email" placeholder="email" />
-
-      <TextInput type="password" name="password" placeholder="password" />
-
+      <TextInput
+        type="text"
+        name="name"
+        placeholder="name"
+        value={values.name}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        error={errors.name && touched.name ? 1 : undefined}
+        errormessage={errors.name}
+      />
+      <TextInput
+        type="text"
+        name="username"
+        placeholder="username"
+        onBlur={handleBlur}
+        onChange={handleChange}
+        value={values.username}
+        error={errors.username && touched.username ? 1 : undefined}
+        errormessage={errors.username}
+      />
+      <TextInput
+        type="text"
+        name="email"
+        placeholder="email"
+        value={values.email}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        error={errors.email && touched.email ? 1 : undefined}
+        errormessage={errors.email}
+      />
+      <TextInput
+        type="password"
+        name="password"
+        placeholder="password"
+        value={values.password}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        error={errors.password && touched.password ? 1 : undefined}
+        errormessage={errors.password}
+      />
       <TextInput
         type="password"
         name="confirmPassword"
         placeholder="confirm password"
+        value={values.confirmPassword}
+        onBlur={handleBlur}
+        onChange={handleChange}
+        error={
+          errors.confirmPassword && touched.confirmPassword ? 1 : undefined
+        }
+        errormessage={errors.confirmPassword}
       />
 
       <button className={styles.signupButton}>Sign Up</button>
